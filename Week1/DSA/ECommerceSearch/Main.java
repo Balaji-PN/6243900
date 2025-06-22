9b984583252b7db587411e6f6c33108e704cfedf
Balaji <120703000+Balaji-PN@users.noreply.github.com>
2025-06-22 09:44:28 +0530

    7 Excercise completed

--- Week1/DSA/ECommerceSearch/Main.java
+++ Week1/DSA/ECommerceSearch/Main.java
@@ -2,9 +2,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package Week1.DSA.InventoryManagement;
+package Week1.DSA.ECommerceSearch;
 
 import java.util.Scanner;
+import Week1.DSA.InventoryManagement.Inventory;
 
 /**
  *
@@ -15,12 +16,14 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         Inventory inventory = new Inventory();
         int opt = 0;
-        while(opt != 5){
+        while(opt != 7){
             System.out.println("1. Add Product");
             System.out.println("2. List all Products");
             System.out.println("3. Update Product");
             System.out.println("4. Remove the Product");
-            System.out.println("5. Exit");
+            System.out.println("5. Linear Search Product by Name");
+            System.out.println("6. Binary Search Product by Name");
+            System.out.println("7. Exit");
 
             opt = sc.nextInt();
             switch(opt){
@@ -46,6 +49,18 @@ public class Main {
                     int id2 = sc.nextInt();
                     inventory.deleteProduct(id2);
                     break;
+                case 5:
+                    System.out.println("Enter product name to search (Linear):");
+                    String nameL = sc.next();
+                    searchConsole.linearSearchByName(inventory, nameL);
+                    break;
+
+                case 6:
+                    System.out.println("Enter product name to search (Binary):");
+                    String nameB = sc.next();
+                    searchConsole.binarySearchByName(inventory, nameB);
+                    break;
+
             }
         }
     }
